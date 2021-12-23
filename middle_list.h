ba084f5cec0c9cac618afa0451088376716ffa91
Boris2006 <92259287+Boris2006@users.noreply.github.com>
2021-12-23 11:40:05 +0300

    Create middle_list.h

---
+++ middle_list.h
@@ -0,0 +1,10 @@
+#ifndef MIDDLE_LIST_H_INCLUDED
+#define MIDDLE_LIST_H_INCLUDED
+using namespace std;
+#include <iostream>
+#include <string>
+#include <vector>
+vector <char> itc_strtlist(string a);
+
+
+#endif // MIDDLE_LIST_H_INCLUDED
